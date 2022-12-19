c6449e7b82eb0064951c21707e41cade4edd834c
Junnu Kyrö <junnukyro@outlook.com>
2022-12-19 12:17:46 +0200

    Versio4

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -6,7 +6,6 @@ const AboutMe = () => {
             <div className='about-content'>
             <h1>About Me</h1>
             <p>I am a second year software engineer student at the Oulu University of Applied Sciences. <br /><br />
-            Before I got selected for Degree Programme in Information Technology, I had zero experience in programming, but now I feel that the days are too short for feeding my raging passion towards it.<br /><br />
             I am eagerly learning new things on software developing/engineering field and right now I am very interested on UI/UX desing and development. I build this portfolio website for showing my projects and telling a bit about myself.</p> 
             </div>
             
